0080c49b8d77dedff02b67e4ab768fc084dc7386
limminjeong98 <limminjeong98@naver.com>
2024-09-17 12:51:41 +0900

    init project

---
+++ src/main/java/com/example/demopaymentsystem/DemoPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demopaymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPaymentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPaymentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demopaymentsystem/DemoPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demopaymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPaymentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
