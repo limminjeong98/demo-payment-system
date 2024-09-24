011f472fa4dcaf554776f93cc4df9c771b06d847
limminjeong98 <limminjeong98@naver.com>
2024-09-24 13:28:13 +0900

    주문 도메인 개발

--- src/main/java/com/example/demopaymentsystem/wallet/WalletRepository.java
+++ src/main/java/com/example/demopaymentsystem/wallet/WalletRepository.java
@@ -1,8 +1,6 @@
 package com.example.demopaymentsystem.wallet;
 
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
